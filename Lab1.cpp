823a0726686eddbae14b1d6e19a4f60244f28dc8
Kolinko-Roman <118218201+Kolinko-Roman@users.noreply.github.com>
2024-09-18 21:12:10 +0300

    Create Lab1.cpp

---
+++ Lab1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+    int number;
+    if(number>0) {
+        cout << "Це число додатнє"; 
+    } else if(number<0) {
+        cout << "Це число відємне";  
+    } else {
+        count << "Це число дорівнює нулю";  
+    }
+    return 0;
+}
