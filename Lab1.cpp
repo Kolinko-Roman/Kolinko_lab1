43206e39d79d68bb8e4dcd0d92959ee7029e56fa
Kolinko Roman <118218201+Kolinko-Roman@users.noreply.github.com>
2024-09-21 10:23:02 +0300

    Update Lab1.cpp

--- Lab1.cpp
+++ Lab1.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <Windows.h>
 using namespace std;
-
 int main()
 {
-    int number;
-    if(number>0) {
-        cout << "Це число додатнє"; 
-    } else if(number<0) {
-        cout << "Це число відємне";  
-    } else {
-        count << "Це число дорівнює нулю";  
-    }
-    return 0;
+	int number;
+	cout << "Введіть любе число:";
+	cin >> number;
+	if(number>0) {
+    	cout << "Це число додатнє";
+	} else if(number<0) {
+    	cout << "Це число відємне";
+	} else {
+    	cout << "Це число дорівнює нулю";
+	}
+	return 0;
 }
